0009818b021d79f3a15c5a074aecd226ca8ae665
Juan Pablo Pérez <juan.perez@lorspi.com>
2023-11-22 08:18:17 -0500

    Asistente con ventanas arreglado

--- actualizar/instalar-actualizar.bat
+++ actualizar/instalar-actualizar.bat
@@ -15,14 +15,38 @@ if "%ruta_actual:~-11%"=="\%carpeta%" (
     call :completa
 
 ) else (
-    echo -----------------------------------------------------------------------------------------------------------
-    echo   ¡ADVERTENCIA!:
-    echo    Para que el modpack funcione correctamente debes ejecutar este archivo dentro de la carpeta %carpeta%.
-    echo    No estás ejecutando este archivo en la carpeta correcta.
-    echo -----------------------------------------------------------------------------------------------------------
-    choice /C SN /M " ¿Deseas continuar en esta carpeta de todos modos? (S/N)"
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  ¡ADVERTENCIA!:                                             │
+    echo │  Para que el modpack funcione correctamente debes           │
+    echo │  ejecutar este archivo dentro de la carpeta %carpeta%.     │
+    echo │  No estás ejecutando este archivo en la carpeta correcta.   │
+    echo │                                                             │
+    echo │  ¿Deseas continuar en esta carpeta de todos modos?          │
+    echo │                                                             │
+    echo │  Escribe S o N                                              │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
+    choice /C SN /M "Escribe S o N" >nul
+
     if errorlevel 2 (
-        echo Has elegido no continuar. Cerrando...
+        cls
+        echo ┌─────────────────────────────────────────────────────────────┐
+        echo │ Modpack - Instalador                                        │
+        echo ╞═════════════════════════════════════════════════════════════╡
+        echo │                                                             │
+        echo │  Has elegido no continuar.                                  │
+        echo │                                                             │
+        echo │                                                             │
+        echo │  Cerrando...                                                │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo └─────────────────────────────────────────────────────────────┘
         timeout /nobreak /t 3 >nul
         exit /b 1
     ) else (
@@ -47,34 +71,88 @@ if exist version.txt (
     fc /b "version.txt" "version_remote.txt" > nul
     REM Verificar el código de error de fc (0 si son idénticos, 1 si son diferentes)
     if errorlevel 1 (
-        echo ------------------------------
-        echo   Iniciando actualización...
-        echo ------------------------------
+        cls
+        echo ┌─────────────────────────────────────────────────────────────┐
+        echo │ Modpack - Instalador                                        │
+        echo ╞═════════════════════════════════════════════════════════════╡
+        echo │                                                             │
+        echo │  Comprobando actualizaciones...                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo └─────────────────────────────────────────────────────────────┘
         timeout /nobreak /t 3 >nul
 
         call :actualizar
 
-        pause
+        pause >nul
         exit
     ) else (
-        echo -----------------------------------------------
-        echo   Tienes la versión mas reciente del modpack.
-        echo -----------------------------------------------
+        cls
+        echo ┌─────────────────────────────────────────────────────────────┐
+        echo │ Modpack - Instalador                                        │
+        echo ╞═════════════════════════════════════════════════════════════╡
+        echo │                                                             │
+        echo │  Comprobando actualizaciones...                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo └─────────────────────────────────────────────────────────────┘
         del /q version_remote.txt
-        pause
+
+        timeout /nobreak /t 2 >nul
+
+        cls
+        echo ┌─────────────────────────────────────────────────────────────┐
+        echo │ Modpack - Instalador                                        │
+        echo ╞═════════════════════════════════════════════════════════════╡
+        echo │                                                             │
+        echo │  Comprobando actualizaciones... OK                          │
+        echo │                                                             │
+        echo │  ¡TIENES LA VERSIÓN MÁS RECIENTE INSTALADA!                 │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │  Pulsa cualquier tecla para cerrar...                       │
+        echo │                                                             │
+        echo └─────────────────────────────────────────────────────────────┘
+        pause >nul
         exit
     )
 ) else (
 
-    echo ----------------------------
-    echo   Iniciando instalación...
-    echo ----------------------------
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Iniciando instalación...                                   │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘    
     timeout /nobreak /t 3 >nul
 
     call :instalar
     call :actualizar
 
-    pause
+    pause >nul
     exit
 )
 
@@ -83,24 +161,82 @@ REM Función instalar
 :instalar
 REM -----------------------------------------------
 
-    echo Descargando archivos iniciales...
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Iniciando instalación... OK                                │
+    echo │  Descargando archivos iniciales...                          │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Descargar el archivo ZIP del repositorio
     curl -s -L -o instalar.zip https://raw.githubusercontent.com/mochos/actualizador/main/instalar/instalar.zip
 
-    echo Extrayendo archivos...
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Iniciando instalación... OK                                │
+    echo │  Descargando archivos iniciales... OK                       │
+    echo │  Extrayendo archivos...                                     │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Descomprimir el archivo ZIP en la carpeta temporal
     tar -xf instalar.zip --strip-components=0
 
     timeout /nobreak /t 3 >nul
 
-    echo Eliminando temporales...
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Iniciando instalación... OK                                │
+    echo │  Descargando archivos iniciales... OK                       │
+    echo │  Extrayendo archivos... OK                                  │
+    echo │  Eliminando temporales...                                   │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Eliminar archivos temporales
     del /q instalar.zip
 
-    echo Archivos iniciales instalados!
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Iniciando instalación... OK                                │
+    echo │  Descargando archivos iniciales... OK                       │
+    echo │  Extrayendo archivos... OK                                  │
+    echo │  Eliminando temporales... OK                                │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
+
+    timeout /nobreak /t 2 >nul
 
     exit /b
 
@@ -118,7 +254,21 @@ REM -----------------------------------------------
     REM Verifica si la carpeta existe
     if exist "mods" (
         pushd "mods"
-        echo Eliminando versiones antiguas...
+        cls
+        echo ┌─────────────────────────────────────────────────────────────┐
+        echo │ Modpack - Instalador                                        │
+        echo ╞═════════════════════════════════════════════════════════════╡
+        echo │                                                             │
+        echo │  Comprobando actualizaciones... OK                          │
+        echo │  Eliminando versiones antiguas...                           │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo │                                                             │
+        echo └─────────────────────────────────────────────────────────────┘
 
         REM Identificar y mover los archivos que comienzan con "NB_" a una carpeta temporal
         mkdir temp
@@ -135,26 +285,84 @@ REM -----------------------------------------------
 
         popd
     )
+    
+    timeout /nobreak /t 1 >nul
 
-    echo Descargando mods...
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Comprobando actualizaciones... OK                          │
+    echo │  Eliminando versiones antiguas... OK                        │
+    echo │  Descargando mods...                                        │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Descargar el archivo ZIP del repositorio
     curl -s -L -o actualizar.zip https://raw.githubusercontent.com/mochos/actualizador/main/actualizar/actualizar.zip
 
-    echo Extrayendo archivos...
+    timeout /nobreak /t 1 >nul
+
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Comprobando actualizaciones... OK                          │
+    echo │  Eliminando versiones antiguas... OK                        │
+    echo │  Descargando mods... OK                                     │
+    echo │  Extrayendo archivos...                                     │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Descomprimir el archivo ZIP en la carpeta temporal
     tar -xf actualizar.zip --strip-components=0
 
-    timeout /nobreak /t 3 >nul
+    timeout /nobreak /t 1 >nul
 
-    echo Eliminando temporales...
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Comprobando actualizaciones... OK                          │
+    echo │  Eliminando versiones antiguas... OK                        │
+    echo │  Descargando mods... OK                                     │
+    echo │  Extrayendo archivos...   OK                                │
+    echo │  Eliminando temporales...                                   │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     REM Eliminar archivos temporales
     del /q actualizar.zip
     del /q version_remote.txt
-    echo --------------------------
-    echo   ¡Instalación completa!
-    echo --------------------------
+    cls
+    echo ┌─────────────────────────────────────────────────────────────┐
+    echo │ Modpack - Instalador                                        │
+    echo ╞═════════════════════════════════════════════════════════════╡
+    echo │                                                             │
+    echo │  Comprobando actualizaciones... OK                          │
+    echo │  Eliminando versiones antiguas... OK                        │
+    echo │  Descargando mods... OK                                     │
+    echo │  Extrayendo archivos...   OK                                │
+    echo │  Eliminando temporales...                                   │
+    echo │                                                             │
+    echo │  ¡INSTALACIÓN TERMINADA!                                    │
+    echo │  Pulsa cualquier tecla para cerrar...                       │
+    echo │                                                             │
+    echo └─────────────────────────────────────────────────────────────┘
 
     exit /b
